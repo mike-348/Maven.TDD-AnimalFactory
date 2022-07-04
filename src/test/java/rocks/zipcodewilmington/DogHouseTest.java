c394c6891dbbe49776ba7bd45041e06b0822788b
mike-348 <michaeljmartin18@gmail.com>
2022-07-04 10:19:06 -0400

    finished doghousetest

--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,5 +1,6 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
@@ -34,6 +35,83 @@ public class DogHouseTest {
 
     @Test
     public void testAddDog() {
+        //given
+        Dog trevor = AnimalFactory.createDog("trevor", new Date());
 
+        //when
+        DogHouse.add(trevor);
+
+        //then
+        Assert.assertEquals(1,(int) DogHouse.getNumberOfDogs());
     }
+
+    @Test
+    public void testAddDog1() {
+        //given
+        Dog trevor = AnimalFactory.createDog("trevor", new Date());
+
+        //when
+        DogHouse.add(trevor);
+
+        //then
+        Assert.assertEquals(trevor,DogHouse.getDogById(0));
+    }
+
+    @Test
+    public void removeDogTest() {
+        //given
+        Dog trevor = AnimalFactory.createDog("trevor", new Date());
+
+        //when
+        DogHouse.add(trevor);
+        DogHouse.remove(trevor);
+
+        //then
+        Assert.assertEquals(0, (int)DogHouse.getNumberOfDogs());
+    }
+
+    @Test
+    public void removeDogTest1() {
+        //given
+        Dog trevor = AnimalFactory.createDog("trevor", new Date());
+        Dog yuri = AnimalFactory.createDog("yuri", new Date());
+
+        //when
+        DogHouse.add(trevor);
+        DogHouse.add(yuri);
+        DogHouse.remove(trevor);
+
+        //then
+        Assert.assertEquals(1, (int)DogHouse.getNumberOfDogs());
+    }
+
+    @Test
+    public void removeDogTest2() {
+        //given
+        Dog trevor = AnimalFactory.createDog("trevor", new Date());
+
+        //when
+        DogHouse.add(trevor);
+        DogHouse.remove(trevor);
+
+        //then
+        Assert.assertEquals(null, DogHouse.getDogById(0));
+    }
+
+    @Test
+    public void getNumberOfDogsTest() {
+        //given
+        Dog trevor = AnimalFactory.createDog("trevor", new Date());
+        Dog yuri = AnimalFactory.createDog("yuri", new Date());
+        Dog tom = AnimalFactory.createDog("tom", new Date());
+
+        //when
+        DogHouse.add(trevor);
+        DogHouse.add(yuri);
+        DogHouse.add(tom);
+
+        //then
+        Assert.assertEquals((Integer) 3, DogHouse.getNumberOfDogs());
+    }
+
 }
